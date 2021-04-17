b2f56ea7ca559cb65e9bf0d833fdf51e18cea3ee
omrfrks <omrfrks310387@gmail.com>
2021-04-17 13:36:25 +0300

    add movie page loading

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,42 +1,46 @@
 <template>
-  <div class="movie">
+  <div>
     <Loaders v-if="$store.state.loading" />
-    <h1>{{ movie.Title }}</h1>
-    <v-img
-      :src="
-        movie.Poster !== 'N/A' ? movie.Poster : 'https://picsum.photos/200/300'
-      "
-      max-height="300"
-      max-width="500"
-    />
-    <v-row align="center" class="mx-0">
-      <v-rating
-        :value="parseFloat(movie.imdbRating)"
-        length="10"
-        color="amber"
-        dense
-        half-increments
-        readonly
-        size="14"
-      ></v-rating>
+    <div v-else class="movie">
+      <h1>{{ movie.Title }}</h1>
+      <v-img
+        :src="
+          movie.Poster !== 'N/A'
+            ? movie.Poster
+            : 'https://picsum.photos/200/300'
+        "
+        max-height="300"
+        max-width="500"
+      />
+      <v-row align="center" class="mx-0">
+        <v-rating
+          :value="parseFloat(movie.imdbRating)"
+          length="10"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
 
-      <div class="grey--text ml-4">
-        {{ movie.imdbRating !== "N/A" ? movie.imdbRating : 0 }} ({{
-          movie.imdbVotes !== "N/A" ? movie.imdbVotes : 0
-        }})
+        <div class="grey--text ml-4">
+          {{ movie.imdbRating !== "N/A" ? movie.imdbRating : 0 }} ({{
+            movie.imdbVotes !== "N/A" ? movie.imdbVotes : 0
+          }})
+        </div>
+      </v-row>
+      <div class="my-4 subtitle-1" v-if="movie.Actors !== 'N/A'">
+        {{ movie.Actors }}
       </div>
-    </v-row>
-    <div class="my-4 subtitle-1" v-if="movie.Actors !== 'N/A'">
-      {{ movie.Actors }}
+      <div v-if="movie.Plot !== 'N/A'">Plot: {{ movie.Plot }}</div>
+      <div>Awards: {{ movie.Awards !== "N/A" ? movie.Awards : "None" }}</div>
+      <div v-if="movie.Country !== 'N/A'">Country: {{ movie.Country }}</div>
+      <div v-if="movie.Director !== 'N/A'">Director: {{ movie.Director }}</div>
+      <div v-if="movie.Genre !== 'N/A'">Genre: {{ movie.Genre }}</div>
+      <div v-if="movie.Language !== 'N/A'">Language: {{ movie.Language }}</div>
+      <div v-if="movie.Writer !== 'N/A'">Writer: {{ movie.Writer }}</div>
+      <div>Year: {{ movie.Year }}</div>
     </div>
-    <div v-if="movie.Plot !== 'N/A'">Plot: {{ movie.Plot }}</div>
-    <div>Awards: {{ movie.Awards !== "N/A" ? movie.Awards : "None" }}</div>
-    <div v-if="movie.Country !== 'N/A'">Country: {{ movie.Country }}</div>
-    <div v-if="movie.Director !== 'N/A'">Director: {{ movie.Director }}</div>
-    <div v-if="movie.Genre !== 'N/A'">Genre: {{ movie.Genre }}</div>
-    <div v-if="movie.Language !== 'N/A'">Language: {{ movie.Language }}</div>
-    <div v-if="movie.Writer !== 'N/A'">Writer: {{ movie.Writer }}</div>
-    <div>Year: {{ movie.Year }}</div>
   </div>
 </template>
 <script>
